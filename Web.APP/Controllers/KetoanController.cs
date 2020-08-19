cc306db6d9bb1094f9c5168ac905631e3d5f6148
letuanacman <letuan.acman@gmail.com>
2020-08-19 18:14:35 +0700

    Tạo giao diện ERP - KeToan

---
+++ Web.APP/Controllers/KetoanController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.APP.Controllers
+{
+    public class KetoanController : Controller
+    {
+        // GET: Ketoan
+        public ActionResult Index()
+        {
+            if (Session["User_DN"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
